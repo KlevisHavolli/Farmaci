fb6e1fabd243d6e73b84f1df0796be9374df1a64
KlevisHavolli <klevis.769@gmail.com>
2024-04-18 01:52:48 +0200

    database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,7 @@
 exports.getProducts =(req, res, next) => {
 
     res.status(200).json({
-        status: true,
+        success: true,
         message: 'This route will show all products in database.'
     })
 }
