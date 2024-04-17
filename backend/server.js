0efa336e62ba900192f81bb3cb51c8cd6e4aa803
KlevisHavolli <klevis.769@gmail.com>
2024-04-18 01:17:22 +0200

    rip

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 
 // Setting up config file
 
-dotenv.config({ path: 'backend/config/config.env'})
+dotenv.config({ path: 'backend/config/config.env' })
 
 app.listen(process.env.PORT,() => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
