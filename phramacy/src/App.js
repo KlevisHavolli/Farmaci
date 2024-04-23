c62feeeedece876807d74074fd1300625d7b29e5
KlevisHavolli <klevis.769@gmail.com>
2024-04-23 13:54:53 +0200

    starting neew loginsignup

--- phramacy/src/App.js
+++ phramacy/src/App.js
@@ -1,8 +1,10 @@
 import  "./App.css";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
 
 export default function App() {
   return (
     <div className="App">
+      <LoginSignup/>
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
     </div>
